91c525bed3da26d5edd3f0f14889c82e8b49ae22
Gulnar-123 <gulnar6816@gmail.com>
2024-07-30 23:01:08 +0530

    shop prj

---
+++ groceryprj/groceryprj/src/main/java/com/grocery/groceryprj/GroceryprjApplication.java
@@ -0,0 +1,13 @@
+package com.grocery.groceryprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryprjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryprjApplication.class, args);
+	}
+
+}
---
+++ groceryprj/groceryprj/src/test/java/com/grocery/groceryprj/GroceryprjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grocery.groceryprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryprjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
